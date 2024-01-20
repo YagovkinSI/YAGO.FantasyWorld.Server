8e28ab1945d700d8bebe019d8c234691dd20dba3
YagovkinSI <sorpionupgrade@mail.ru>
2024-01-20 06:33:32 +0400

    [TASK-1] Use dotnet format

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using YAGO.FantasyWorld.Server.Application.WeatherForecastService;
 
 namespace YAGO.FantasyWorld.Server.Host
@@ -24,7 +17,6 @@ namespace YAGO.FantasyWorld.Server.Host
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             AddAppServices(services);
@@ -36,12 +28,8 @@ namespace YAGO.FantasyWorld.Server.Host
             });
         }
 
-        private static void AddAppServices(IServiceCollection services)
-        {
-            services.AddScoped<WeatherForecastService>();
-        }
+        private static void AddAppServices(IServiceCollection services) => services.AddScoped<WeatherForecastService>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
